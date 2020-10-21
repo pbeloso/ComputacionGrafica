98689e36bca56313faee63231e709c05043a7a50
pbeloso <pbeloso96@gmail.com>
2020-10-21 18:09:11 -0300

    Muchas cosas

---
+++ obj.py
@@ -0,0 +1,53 @@
+class obj:
+    def __init__(self):
+        self.vertFaces = []
+        self.normalFaces = []
+        self.texturesFaces = []
+
+    def objParser(self, path):
+        objFile = open(path, 'r')
+        #objFile = open('box.obj', 'r')
+
+        vertexList = []
+        normalList = []
+        textList = []
+
+        for line in objFile:
+            split = line.split()
+            if not len(split) or split[0] == "o" or split[0] == "#":	# Si es un espacio en blanco, #, o;  continuo
+                continue
+
+            if split[0] == "v":			 	# Si empieza con v, lo agrego a la lista de vertices				
+                vertex = [ float(split[1]), float(split[2]), float(split[3]) ]
+                vertexList.append(vertex)
+            
+            if split[0] == "vn":			# Si empieza con vn, lo agrego a la lista de normales	
+                normal = [ float(split[1]), float(split[2]), float(split[3]) ]
+                normalList.append(normal)
+
+            if split[0] == "vt":            # Si empieza con vn, lo agrego a la lista de normales				
+                texture = [ float(split[1]), float(split[2]) ]
+                textList.append(texture)
+
+
+            elif split[0] == "f":			# Si empieza con f, lo agrego a la lista de faces
+                vert = []
+                norm = []
+                text = []
+
+                for i in range(1,4):
+                    splitFace = split[i].split("/")
+                    vert.append(splitFace[0])
+                    norm.append(splitFace[1])
+                    text.append(splitFace[2])
+
+                vertexface = [ vertexList[int(vert[0])-1], vertexList[int(vert[1])-1], vertexList[int(vert[2])-1] ]
+                self.vertFaces.append(vertexface)
+
+                normalFace = [ normalList[int(norm[0])-1], normalList[int(norm[1])-1], normalList[int(norm[2])-1] ]
+                self.normalFaces.append(normalFace)
+
+                textFace = [ textList[int(text[0])-1], textList[int(text[1])-1], textList[int(text[2])-1] ]
+                self.texturesFaces.append(textFace)
+            
+
--- practico.py
+++ practico.py
@@ -1,49 +1,68 @@
-import pygame
-from pygame.locals import *
-
-from OpenGL.GL import *
 import numpy
+import pygame
+
+from pygame.locals import *
+from OpenGL.GL import *
+from obj import *
 
 def main():
+
+    #--------------------------------------------------------------------------------------- 
+
+    # Instrucciones para levantar ventana grafica
+    
     pygame.init()
     cw = 800
     ch = 600
     display = (cw,ch)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
 
+    #---------------------------------------------------------------------------------------  
+
+    # Cargar archivos .obj.
+      
+    knight = obj()
+    knight.objParser("./Animaciones/knight_animado/knight_stand_0.obj")
+
+    weapon_k = obj()
+    weapon_k.objParser("./Animaciones/weapon_knight_animada/weapon_k_stand_0.obj")
+
+    hueteotl = obj()
+    hueteotl.objParser("./Animaciones/hueteotl_animado/hueteotl_stand_0.obj")
+
+    weapon_h = obj()
+    weapon_h.objParser("./Animaciones/weapon_hueteotl_animada/weapon_stand_0.obj")
+    
     #---------------------------------------------------------------------------------------
 
-    vertFaces = []
-    normalFaces = []
-    texturesFaces = []
+    # Activo las texturas ( 8 disponibles).
 
-    vertFacesW = []
-    normalFacesW = []
-    texturesFacesW = []
-         
-    objParser("./Animaciones/knight_animado/knight_stand_0.obj", vertFaces, normalFaces, texturesFaces)       # Llamo al parser.
-    objParser("./Animaciones/weapon_knight_animada/weapon_k_stand_0.obj", vertFacesW, normalFacesW, texturesFacesW)    # Llamo al parser.
+    glEnable(GL_TEXTURE_2D)                         
+    glActiveTexture(GL_TEXTURE0)                   
 
-    #---------------------------------------------------------------------------------------
-
-    glEnable(GL_TEXTURE_2D)                         # Activo el manejo de texturas
-    glActiveTexture(GL_TEXTURE0)                    # Activo la textura 0 (hay 8 disponibles)
-    text = loadTexture("./knight_good.png")         # Llamo a la funcion que levanta la textura a memoria de video
+    text = loadTexture("./knight_good.png")         # Funcion que levanta la textura a memoria de video
 
     text2 = loadTexture("./knight.png")         
 
     text3 = loadTexture("./Animaciones/weapon_knight_animada/weapon_k.png")
 
+    text5 = loadTexture("./Animaciones/hueteotl_animado/hueteotl.png")
+
+    text6 = loadTexture("./Animaciones/weapon_hueteotl_animada/weapon.png")
+
     #---------------------------------------------------------------------------------------
     
-    glShadeModel(GL_SMOOTH) # El pipeline estatico use Gouraud Shading (sombreado suave).
+    #glShadeModel(GL_SMOOTH) # El pipeline estatico use Gouraud Shading (sombreado suave).
 
-    #Empiezo por setear los parametros del material del objeto a dibujar.
+    # Seteao los parametros del material del objeto a dibujar.
 
     glMaterial(GL_FRONT_AND_BACK, GL_DIFFUSE, [0.4, 0.4, 0.4,1]) # Color difuso.
     glMaterial(GL_FRONT_AND_BACK, GL_AMBIENT, [0.1,0.1,0.1,1])   # Color ambiente (iluminacion indirecta).
     glMaterial(GL_FRONT_AND_BACK, GL_SPECULAR, [1,1,1,1])        # Color especular, para la parte de Lambert extendida por Phong.
     glMaterial(GL_FRONT_AND_BACK, GL_SHININESS, 32)              # Coeficiente especular.
+
+
+    # Activo las luces ( 0 a 7 )
 
     glEnable(GL_LIGHT0)   
     glLight(GL_LIGHT0, GL_DIFFUSE, [1,1,1,1])       # Color difuso de la luz (intensidad y color).
@@ -52,7 +71,7 @@ def main():
     glLight(GL_LIGHT0, GL_AMBIENT, [0.1,0.1,0.1,1]) # Cuanto aporta esta luz a la intensidad ambiente (iluminacion indirecta).
     glLight(GL_LIGHT0, GL_SPECULAR, [1,1,1,1])      # El color especular de la luz.
 
-    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_DEPTH_TEST)                         # Comparaciones de profundidad y actualizar el bufer de profundidad.
     
     #---------------------------------------------------------------------------------------
 
@@ -68,6 +87,11 @@ def main():
     ang = 0.0
     vel = 0.0
 
+    mode = GL_FILL
+    zBuffer = True
+    bfc = False
+    bfcCW = True
+
     light = False
     wire = False
 
@@ -75,61 +99,79 @@ def main():
     
     while True:
         
-        for event in pygame.event.get():        # Chequeo todos los eventos del cuadro anterior, utilizando la libreria de pygame.
-            if event.type == pygame.QUIT:       # Quiere cerrar la ventana, y la aplicacion debe finalizar.
+        for event in pygame.event.get():        
+            if event.type == pygame.QUIT:       
                 pygame.quit()
                 quit()
-            if event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYDOWN:    # Evento tecla presionada.
+
                 if event.key == pygame.K_ESCAPE:
                     pygame.quit()
                     quit()
-                if event.key == pygame.K_l:
-                    #Con la letra L prendo y apago la iluminacion
+
+                if event.key == pygame.K_l:     # Con la letra L prendo y apago la iluminacion
                     light = not light
                     if(light):
                         glEnable(GL_LIGHTING)
                     else:
                         glDisable(GL_LIGHTING)
-                if event.key == pygame.K_m:   # Si el evento apreta letra m, lo deja en formato malla o no (con o sin fondo).
-                    if not wire:
-                        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+
+                if event.key == pygame.K_m:     # Con la letra m, lo deja en formato malla o no (con o sin fondo).
+                    if mode == GL_LINE:
+                        mode = GL_FILL
                     else:
-                        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
-                    wire = not wire
-            if event.type == pygame.KEYDOWN:    #Si el evento es una tecla presionada.
-                if event.key == pygame.K_LEFT:
-                    vel = 1
-                elif event.key == pygame.K_RIGHT:
-                    vel = -1
+                        mode = GL_LINE
+                    glPolygonMode( GL_FRONT_AND_BACK, mode)
+
+                if event.key == pygame.K_z:     # Con la letra z, activa el z-buffer
+                    zBuffer = not zBuffer
+                    if(zBuffer):
+                        glEnable(GL_DEPTH_TEST)
+                    else:
+                        glDisable(GL_DEPTH_TEST)
+
+                if event.key == pygame.K_b:     # Con la letra b, activo cullface
+                    bfc = not bfc
+                    if(bfc):
+                        glEnable(GL_CULL_FACE)
+                    else:
+                        glDisable(GL_CULL_FACE)
+
+                if event.key == pygame.K_c:     # Con la letra c
+                    bfcCW = not bfcCW
+                    if(bfcCW):
+                        glFrontFace(GL_CW)
+                    else:
+                        glFrontFace(GL_CCW)
+
+    #---------------------------------------------------------------------------------------
     
-    
-        
         glMatrixMode(GL_MODELVIEW)                          # Activo el stack de matrices MODELVIEW.           
         glLoadIdentity()                                    # Limpio todas la transformaciones previas.
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)    # Limpio el buffer de colores donde voy a dibujar.
         
-        glEnableClientState(GL_VERTEX_ARRAY)                # Habilito el array de vertices.
-        glEnableClientState(GL_NORMAL_ARRAY)                # Habilito el array de normales.
-        glEnableClientState(GL_TEXTURE_COORD_ARRAY)         # Habilito el array de coordenadas de textura
+        # Habilito arrays.
 
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)    # Limpio el buffer de colores donde voy a dibujar. 
+        glEnableClientState(GL_VERTEX_ARRAY)                
+        glEnableClientState(GL_NORMAL_ARRAY)               
+        glEnableClientState(GL_TEXTURE_COORD_ARRAY)        
 
     #---------------------------------------------------------------------------------------    
 
         glPushMatrix()
 
-        glTranslatef(-30,0,-60)                               # Traslacion. (derecha, arriba, hacia adentro).
-        #glTranslatef(0,0,-3)
+        glTranslatef(-30,0,-60) # Traslacion (derecha, arriba, hacia adentro).
 
-        glRotatef(-90, 1,0,0)                               # Rotacion. (angulo, eje x, eje y, eje z).
-        #glRotatef(ang, 0,0,1)
-        #ang += 0.5
 
-        glVertexPointer(3, GL_FLOAT, 0, vertFaces)          # Paso el array de vertices, de 3 coords en cada elemento, que son de tipo float.
-        glNormalPointer(GL_FLOAT, 0, normalFaces)           # Paso el array de normales.
-        glTexCoordPointer(2, GL_FLOAT, 0, texturesFaces)    # Paso la lista de coordenadas de textura para cada vertice
+        glRotatef(-90, 1,0,0)   # Rotacion (angulo, eje x, eje y, eje z).
 
-        glBindTexture(GL_TEXTURE_2D, text)                  # Cargo la textura "text" en la posicion activa (que es la 0 en este ejemplo)
-        glDrawArrays(GL_TRIANGLES, 0, len(vertFaces)*3)     # Indico que voy a dibujar triangulos, usando el array de vertices.
+
+        glVertexPointer(3, GL_FLOAT, 0, knight.vertFaces)          
+        glNormalPointer(GL_FLOAT, 0, knight.normalFaces)           
+        glTexCoordPointer(2, GL_FLOAT, 0, knight.texturesFaces)    
+
+        glBindTexture(GL_TEXTURE_2D, text)                         
+        glDrawArrays(GL_TRIANGLES, 0, len(knight.vertFaces)*3)    
 
         glPopMatrix()
 
@@ -137,19 +179,17 @@ def main():
 
         glPushMatrix()
 
-        glTranslatef(-30,0,-60)                               # Traslacion. (derecha, arriba, hacia adentro).
-        #glTranslatef(0,0,-3)
+        glTranslatef(-30,0,-60) # Traslacion. (derecha, arriba, hacia adentro).
 
-        glRotatef(-90, 1,0,0)                               # Rotacion. (angulo, eje x, eje y, eje z).
-        #glRotatef(ang, 0,0,1)
-        #ang += 0.5
 
-        glVertexPointer(3, GL_FLOAT, 0, vertFacesW)          # Paso el array de vertices, de 3 coords en cada elemento, que son de tipo float.
-        glNormalPointer(GL_FLOAT, 0, normalFacesW)           # Paso el array de normales.
-        glTexCoordPointer(2, GL_FLOAT, 0, texturesFacesW)    # Paso la lista de coordenadas de textura para cada vertice
+        glRotatef(-90, 1,0,0)   # Rotacion. (angulo, eje x, eje y, eje z).
 
-        glBindTexture(GL_TEXTURE_2D, text3)                  # Cargo la textura "text" en la posicion activa (que es la 0 en este ejemplo)
-        glDrawArrays(GL_TRIANGLES, 0, len(vertFacesW)*3)     # Indico que voy a dibujar triangulos, usando el array de vertices.
+        glVertexPointer(3, GL_FLOAT, 0, weapon_k.vertFaces)          
+        glNormalPointer(GL_FLOAT, 0, weapon_k.normalFaces)           
+        glTexCoordPointer(2, GL_FLOAT, 0, weapon_k.texturesFaces)   
+
+        glBindTexture(GL_TEXTURE_2D, text3)      
+        glDrawArrays(GL_TRIANGLES, 0, len(weapon_k.vertFaces)*3)     
 
         glPopMatrix()
 
@@ -158,16 +198,16 @@ def main():
         glPushMatrix()
 
         glTranslatef(30,0,-60)
-        glRotatef(-90, 1,0,0)                               # Rotacion. (angulo, eje x, eje y, eje z).
-        glRotatef(243, 0,0,1)
+        glRotatef(-90, 1,0,0)   # Rotacion. (angulo, eje x, eje y, eje z).
+        glRotatef(230, 0,0,1)
         
 
-        glVertexPointer(3, GL_FLOAT, 0, vertFaces)          # Paso el array de vertices, de 3 coords en cada elemento, que son de tipo float.
-        glNormalPointer(GL_FLOAT, 0, normalFaces)           # Paso el array de normales.
-        glTexCoordPointer(2, GL_FLOAT, 0, texturesFaces)    # Paso la lista de coordenadas de textura para cada vertice
+        glVertexPointer(3, GL_FLOAT, 0, hueteotl.vertFaces)         
+        glNormalPointer(GL_FLOAT, 0, hueteotl.normalFaces)           
+        glTexCoordPointer(2, GL_FLOAT, 0, hueteotl.texturesFaces)    
 
-        glBindTexture(GL_TEXTURE_2D, text2)                  # Cargo la textura "text" en la posicion activa (que es la 0 en este ejemplo)
-        glDrawArrays(GL_TRIANGLES, 0, len(vertFaces)*3)     # Indico que voy a dibujar triangulos, usando el array de vertices.
+        glBindTexture(GL_TEXTURE_2D, text5)                  
+        glDrawArrays(GL_TRIANGLES, 0, len(hueteotl.vertFaces)*3)     
 
         glPopMatrix()
 
@@ -176,78 +216,34 @@ def main():
         glPushMatrix()
 
         glTranslatef(30,0,-60)
-        glRotatef(-90, 1,0,0)                               # Rotacion. (angulo, eje x, eje y, eje z).
+        glRotatef(-90, 1,0,0)   # Rotacion. (angulo, eje x, eje y, eje z).
         glRotatef(243, 0,0,1)
         
 
-        glVertexPointer(3, GL_FLOAT, 0, vertFacesW)          # Paso el array de vertices, de 3 coords en cada elemento, que son de tipo float.
-        glNormalPointer(GL_FLOAT, 0, normalFacesW)           # Paso el array de normales.
-        glTexCoordPointer(2, GL_FLOAT, 0, texturesFacesW)    # Paso la lista de coordenadas de textura para cada vertice
+        glVertexPointer(3, GL_FLOAT, 0, weapon_h.vertFaces)          
+        glNormalPointer(GL_FLOAT, 0, weapon_h.normalFaces)           
+        glTexCoordPointer(2, GL_FLOAT, 0, weapon_h.texturesFaces)    
 
-        glBindTexture(GL_TEXTURE_2D, text3)                  # Cargo la textura "text" en la posicion activa (que es la 0 en este ejemplo)
-        glDrawArrays(GL_TRIANGLES, 0, len(vertFacesW)*3)     # Indico que voy a dibujar triangulos, usando el array de vertices.
+        glBindTexture(GL_TEXTURE_2D, text6)                  
+        glDrawArrays(GL_TRIANGLES, 0, len(weapon_h.vertFaces)*3)     
 
         glPopMatrix()
 
     #---------------------------------------------------------------------------------------
     
-        glBindTexture(GL_TEXTURE_2D, 0)                     # Luego de dibujar, desactivo la textura
+        # Luego de dibujar, desactivo todo.
+
+        glBindTexture(GL_TEXTURE_2D, 0)                     
 
         glDisableClientState(GL_VERTEX_ARRAY)
         glDisableClientState(GL_NORMAL_ARRAY)
         glDisableClientState(GL_TEXTURE_COORD_ARRAY)
         
-        pygame.display.flip()                               # Hago flip de los buffers, para que se refresque la imagen en pantalla
+        pygame.display.flip()       # Hago flip de los buffers, para que se refresque la imagen en pantalla
 
     glDeleteTextures([text])
     pygame.quit()
     quit()
-
-def objParser(path, vertFaces, normalFaces, texturesFaces):
-    objFile = open(path, 'r')
-    #objFile = open('box.obj', 'r')
-
-    vertexList = []
-    normalList = []
-    textList = []
-
-    for line in objFile:
-        split = line.split()
-        if not len(split) or split[0] == "o" or split[0] == "#":	# Si es un espacio en blanco, #, o;  continuo
-            continue
-
-        if split[0] == "v":			 	# Si empieza con v, lo agrego a la lista de vertices				
-            vertex = [ float(split[1]), float(split[2]), float(split[3]) ]
-            vertexList.append(vertex)
-        
-        if split[0] == "vn":			# Si empieza con vn, lo agrego a la lista de normales	
-            normal = [ float(split[1]), float(split[2]), float(split[3]) ]
-            normalList.append(normal)
-
-        if split[0] == "vt":            # Si empieza con vn, lo agrego a la lista de normales				
-            texture = [ float(split[1]), float(split[2]) ]
-            textList.append(texture)
-
-
-        elif split[0] == "f":			# Si empieza con f, lo agrego a la lista de faces
-            vert = []
-            norm = []
-            text = []
-
-            for i in range(1,4):
-                splitFace = split[i].split("/")
-                vert.append(splitFace[0])
-                norm.append(splitFace[1])
-                text.append(splitFace[2])
-
-            vertexface = [ vertexList[int(vert[0])-1], vertexList[int(vert[1])-1], vertexList[int(vert[2])-1] ]
-            vertFaces.append(vertexface)
-
-            normalFace = [ normalList[int(norm[0])-1], normalList[int(norm[1])-1], normalList[int(norm[2])-1] ]
-            normalFaces.append(normalFace)
-
-            textFace = [ textList[int(text[0])-1], textList[int(text[1])-1], textList[int(text[2])-1] ]
-            texturesFaces.append(textFace)
 
 def loadTexture(path):
     #Cargo la imagen a memoria. pygame se hace cargo de decodificarla correctamente
@@ -283,5 +279,4 @@ def loadTexture(path):
     #devuelvo el identificador de la textura para que pueda ser usada mas adelante
     return texid
 
-    
 main()  
