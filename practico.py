6547e68af7d9fc9a5f2aa4fbdb68979193b9c3e8
pbeloso <pbeloso96@gmail.com>
2020-10-21 21:20:25 -0300

    Update practico.py

--- practico.py
+++ practico.py
@@ -3,6 +3,8 @@ import pygame
 
 from pygame.locals import *
 from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GL.shaders import *
 from obj import *
 
 def main():
@@ -38,9 +40,11 @@ def main():
     # Activo las texturas ( 8 disponibles).
 
     glEnable(GL_TEXTURE_2D)                         
-    glActiveTexture(GL_TEXTURE0)                   
+    glActiveTexture(GL_TEXTURE0)       
 
-    text = loadTexture("./knight_good.png")         # Funcion que levanta la textura a memoria de video
+    # Funcion que levanta la textura a memoria de video            
+
+    text = loadTexture("./knight_good.png")         
 
     text2 = loadTexture("./knight.png")         
 
@@ -55,30 +59,28 @@ def main():
     #glShadeModel(GL_SMOOTH) # El pipeline estatico use Gouraud Shading (sombreado suave).
 
     # Seteao los parametros del material del objeto a dibujar.
-
-    glMaterial(GL_FRONT_AND_BACK, GL_DIFFUSE, [0.4, 0.4, 0.4,1]) # Color difuso.
-    glMaterial(GL_FRONT_AND_BACK, GL_AMBIENT, [0.1,0.1,0.1,1])   # Color ambiente (iluminacion indirecta).
-    glMaterial(GL_FRONT_AND_BACK, GL_SPECULAR, [1,1,1,1])        # Color especular, para la parte de Lambert extendida por Phong.
-    glMaterial(GL_FRONT_AND_BACK, GL_SHININESS, 32)              # Coeficiente especular.
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, [1,1,1,1])
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, [0.2,0.2,0.2,1])
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, [1,1,1,1])
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 128)           
 
 
     # Activo las luces ( 0 a 7 )
 
     glEnable(GL_LIGHT0)   
-    glLight(GL_LIGHT0, GL_DIFFUSE, [1,1,1,1])       # Color difuso de la luz (intensidad y color).
-    glLight(GL_LIGHT0, GL_POSITION, [0,0,0,1])      # En que parte de la escena se encuentra el foco, con luz puntual.
-    #glLight(GL_LIGHT0, GL_POSITION, [0,0,0,0])     # En que parte de la escena se encuentra el foco, con luz direccional.
-    glLight(GL_LIGHT0, GL_AMBIENT, [0.1,0.1,0.1,1]) # Cuanto aporta esta luz a la intensidad ambiente (iluminacion indirecta).
-    glLight(GL_LIGHT0, GL_SPECULAR, [1,1,1,1])      # El color especular de la luz.
+    glLight(GL_LIGHT0, GL_DIFFUSE, [1.0,0.0,0.0,1])      
+    glLight(GL_LIGHT0, GL_AMBIENT, [1,1,1,1])       
+    glLight(GL_LIGHT0, GL_POSITION, [0,100,-50,0])      # [0,0,0,1] es luz puntual, [0,0,0,0] es luz direccional
+    glLight(GL_LIGHT0, GL_SPECULAR, [1,0,0,1])
 
-    glEnable(GL_DEPTH_TEST)                         # Comparaciones de profundidad y actualizar el bufer de profundidad.
-    
     #---------------------------------------------------------------------------------------
 
     glMatrixMode(GL_PROJECTION)          # Activo el stack de matrices para la proyeccion.
-    glLoadIdentity()                    # Cargo una identidad para asegurarme que comience vacio.
+    glLoadIdentity()                     # Cargo una identidad para asegurarme que comience vacio.
     glViewport(0,0,cw*2,ch*2)            # Crea la matriz de escala, transforma de unidades arbitrarias a pixels.
     glFrustum(-1, 1, -1, 1, 1, 1000)     # Crea la matriz de Proyeccion. volumen de vista.
+
+    glEnable(GL_DEPTH_TEST)                         # Comparaciones de profundidad y actualizar el bufer de profundidad.
 
     #glClearColor(0,0,1,1)               # Color de fondo.
     #---------------------------------------------------------------------------------------
@@ -91,9 +93,8 @@ def main():
     zBuffer = True
     bfc = False
     bfcCW = True
-
     light = False
-    wire = False
+
 
     #---------------------------------------------------------------------------------------
     
@@ -246,37 +247,23 @@ def main():
     quit()
 
 def loadTexture(path):
-    #Cargo la imagen a memoria. pygame se hace cargo de decodificarla correctamente
-    surf = pygame.image.load(path)
-    surf = pygame.transform.flip(surf, False, True) #espejar la imagen
-    #Obtengo la matriz de colores de la imagen en forma de un array binario
-    #Le indico el formato en que quiero almacenar los datos (RGBA) y que invierta la matriz, para poder usarla correctamente con OpenGL
-    image = pygame.image.tostring(surf, 'RGBA', 1)
-    #Obentego las dimensiones de la imagen
-    ix, iy = surf.get_rect().size
-    #Creo una textura vacia en memoria de video, y me quedo con el identificador (texid) para poder referenciarla
-    texid = glGenTextures(1)
-    #Activo esta nueva textura para poder cargarle informacion
-    glBindTexture(GL_TEXTURE_2D, texid)
-    #Seteo los tipos de filtro a usar para agrandar y achivar la textura
+
+    surf = pygame.image.load(path)                  # Cargo imagen en memoria
+    surf = pygame.transform.flip(surf, False, True) # Espejo la imagen
+
+    image = pygame.image.tostring(surf, 'RGBA', 1)  # Obtengo la matriz de colores de la imagen.
+    
+    ix, iy = surf.get_rect().size       # Obentego las dimensiones de la imagen
+    texid = glGenTextures(1)            # Textura vacia en memoria de video
+    glBindTexture(GL_TEXTURE_2D, texid) # Activo textura
+
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-    #Cargo la matriz de colores dentro de la textura
-    #Los parametros que le paso son:
-    # - Tipo de textura, en este caso GL_TEXTURE_2D
-    # - Nivel de mipmap, en este caso 0 porque no estoy usando mas niveles
-    # - Formato en que quiero almacenar los datos en memoria de video, GL_RGB en este caso, porque no necesito canal Alfa
-    # - Ancho de la textura
-    # - Alto de la textura
-    # - Grosor en pixels del borde, en este caso 0 porque no quiero agregar borde a al imagen
-    # - Formato de los datos de la imagen, en este caso GL_RGBA que es como lo leimos con pygame.image
-    # - Formato de los canales de color, GL_UNSIGNED_BYTE quiere decir que son 8bits para cada canal
-    # - La imagen, en este caso la matriz de colores que creamos con pygame.image.tostring
+  
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, ix, iy, 0, GL_RGBA, GL_UNSIGNED_BYTE, image)
-    #Una vez que tengo todo cargado, desactivo la textura para evitar que se dibuje por error mas adelante
-    #Cada vez que quiera usarla, puedo hacer glBindTexture con el identificador (texid) que me guarde al crearla
+    
     glBindTexture(GL_TEXTURE_2D, 0)
-    #devuelvo el identificador de la textura para que pueda ser usada mas adelante
+    
     return texid
 
 main()  
